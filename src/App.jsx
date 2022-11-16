2825ac2be8811f910410505ef4942daf0a817b0d
kodeman2 <holuwakoladey@gmail.com>
2022-11-16 20:30:23 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+import Hero from "./Hero";
 import NavBar from "./NavBar";
 function App() {
   return (
     <div className=" w-full">
-      
       <NavBar />
+      <Hero/>
     </div>
   );
 }
---
+++ src/Hero.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function Hero() {
+  return (
+    <div className='w-full h-[300px] bg-slate-300'>
+      <div className='grid grid-cols-2 justify-between px-8'>
+
+       <div>
+         <h1 className='text-2xl text-white font-bold py-8'>Learn to code by <br/> watching others</h1>
+         <p className='text-white py-3'>See how experienced developers solve problems in real-time.</p>
+         <button className='bg-white text-green-500 font-bold px-4 py-2 rounded-lg'>Try it free 7 days</button>
+
+
+       </div>
+
+       <div className='flex justify-center items-center'>
+        {/*vector illustration online*/}
+         
+         
+        
+         
+
+         
+
+         
+     
+        
+
+       </div>
+       </div>
+
+    </div>
+  )
+}
