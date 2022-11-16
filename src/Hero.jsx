d56b76e2bf3f1ece85af81e8ba517ad64325178b
kodeman2 <holuwakoladey@gmail.com>
2022-11-16 20:38:59 +0100

    updates

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,35 +1,30 @@
-import React from 'react'
+import React from "react";
 
 export default function Hero() {
   return (
-    <div className='w-full h-[300px] bg-slate-300'>
-      <div className='grid grid-cols-2 justify-between px-8'>
+    <div className="w-full h-[300px] bg-slate-300">
+      <div className="grid grid-cols-2 justify-between px-8">
+        <div>
+          <h1 className="text-2xl text-white font-bold py-8">
+            Learn to code by <br /> watching others
+          </h1>
+          <p className="text-white py-3">
+            See how experienced developers solve problems in real-time.
+          </p>
+          <button className="bg-white text-green-500 font-bold px-4 py-2 rounded-lg">
+            Try it free 7 days
+          </button>
+        </div>
 
-       <div>
-         <h1 className='text-2xl text-white font-bold py-8'>Learn to code by <br/> watching others</h1>
-         <p className='text-white py-3'>See how experienced developers solve problems in real-time.</p>
-         <button className='bg-white text-green-500 font-bold px-4 py-2 rounded-lg'>Try it free 7 days</button>
-
-
-       </div>
-
-       <div className='flex justify-center items-center'>
-        {/*vector illustration online*/}
-        <img src="./src/assets/heronew.png" alt="hero" className=' flex justify-center ml-10 w-[200px] h-[200px]'/>
-         
-         
-        
-         
-
-         
-
-         
-     
-        
-
-       </div>
-       </div>
-
+        <div className="flex justify-center items-center">
+          {/*vector illustration online*/}
+          <img
+            src="./src/assets/heronew.png"
+            alt="hero"
+            className="px-4 py-4 ml-10 w-[200px] h-[200px]"
+          />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
